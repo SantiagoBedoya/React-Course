1026fe4b2d07969f1379778ee9ed3ddaa80c7305
SantiagoBedoya <a.sbedoya200110@gmail.com>
2021-01-05 21:05:35 -0500

    React Course

---
+++ 09-calendar-app/src/types/types.js
@@ -0,0 +1,21 @@
+export const types = {
+    uiOpenModal: '[UI] Open Modal',
+    uiCloseModal: '[UI] Close Modal',
+
+    eventStartAddNew: '[EVENT] Start Add New',
+    eventLogout: '[EVENT] Logout event',
+    eventAddNew: '[EVENT] Add New',
+    eventSetActive: '[EVENT] Set New',
+    eventClearActiveEvent: '[EVENT] Clear active event',
+    eventUpdated: '[EVENT] Event updated',
+    eventDeleted: '[EVENT] Event deleted',
+    eventLoaded: '[EVENT] Events loaded',
+
+    authChekingFinish: '[AUTH] Finish checking login state',
+    authStartCheking: '[AUTH] Start login',
+    authStartLogin: '[AUTH] Start login',
+    authLogin: '[AUTH] Login',
+    authStartRegister: '[AUTH] Start Register',
+    authStartTokenRenew: '[AUTH] Start token renew',
+    authLogout: '[AUTH] Logout'
+}
